7a7c95da16fe2cbe93feb75d623dba02c4a21fe5
mohammad-a-habib <92282302+mohammad-a-habib@users.noreply.github.com>
2021-10-10 22:06:33 -0400

    Add files via upload

---
+++ IPS_Final_Submission/src/com/habib/Main.java
@@ -0,0 +1,9 @@
+package com.habib;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PatientProfInterface test = new PatientProfInterface("PatientFile_List.txt");
+        test.getUserChoice();
+    }
+}
